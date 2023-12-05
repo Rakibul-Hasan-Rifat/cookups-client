d8033d001e587f8cb9fb2f6c8334b2fd03c38f2f
Rakibul Hasan <rakibulhasan745101@gmail.com>
2023-12-05 17:01:41 +0600

    handling the food variant having nothing in stock

--- src/pages/TypedFoodsPage/TypedFoodsPage.jsx
+++ src/pages/TypedFoodsPage/TypedFoodsPage.jsx
@@ -10,7 +10,7 @@ const TypedFoodsPage = () => {
     const [loadedFoodType, setLoadedFoodType] = useState([])
 
     useEffect(() => {
-        fetch(`http://localhost:5000/foodTypes/${params.type}`)
+        loadedFoods?.length > 0 && fetch(`http://localhost:5000/foodTypes/${params.type}`)
             .then(res => res.json())
             .then(data => {
                 setLoadedFoodType(data)
@@ -18,42 +18,54 @@ const TypedFoodsPage = () => {
             .catch(err => {
                 console.log(err)
             })
-    }, [params.type])
+    }, [params.type, loadedFoods])
 
     console.log(loadedFoods)
-    console.log('10 typedfoodspage',params.type)
+    console.log('10 typedfoodspage', params.type)
 
     return (
         <>
-            <section style={{ margin: '120px auto 0', width: '90%' }}>
-                <div style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                    <span
-                        onClick={() => setSliderCounter(sliderCounter < 2 ? 3 : --sliderCounter)}
-                        style={{ fontSize: '100px', position: 'absolute', left: '30px', color: '#fff', backgroundColor: 'rgba(79, 182, 92, .3)', borderRadius: '8px', padding: '0 20px', cursor: 'pointer' }}
-                    >
-                        {'<'}
-                    </span>
-                    <img src={loadedFoodType.images && loadedFoodType?.images[sliderCounter]} style={{ width: '100%' }} alt="" />
-                    <span
-                        onClick={() => setSliderCounter(sliderCounter > 2 ? 1 : ++sliderCounter)}
-                        style={{ fontSize: '100px', position: 'absolute', right: '30px', color: '#fff', backgroundColor: 'rgba(79, 182, 92, .3)', borderRadius: '8px', padding: '0 20px', cursor: 'pointer' }}
-                    >
-                        {'>'}
-                    </span>
-                </div>
-            </section>
-            <section style={{ width: '90%', margin: '120px auto', display: 'grid', gap: '30px', gridTemplateColumns: 'repeat(2, 1fr)' }}>
-                {
-                    loadedFoods?.map(
-                        singleFood => (
-                            <SingleFood
-                                key={singleFood._id}
-                                singleFood={singleFood}
-                            />
-                        )
+            {
+                loadedFoods?.length > 0 ? (
+                    <>
+                        <section style={{ margin: '120px auto 0', width: '90%' }}>
+                            <div style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                <span
+                                    onClick={() => setSliderCounter(sliderCounter < 2 ? 3 : --sliderCounter)}
+                                    style={{ fontSize: '100px', position: 'absolute', left: '30px', color: '#fff', backgroundColor: 'rgba(79, 182, 92, .3)', borderRadius: '8px', padding: '0 20px', cursor: 'pointer' }}
+                                >
+                                    {'<'}
+                                </span>
+                                <img src={loadedFoodType.images && loadedFoodType?.images[sliderCounter]} style={{ width: '100%' }} alt="" />
+                                <span
+                                    onClick={() => setSliderCounter(sliderCounter > 2 ? 1 : ++sliderCounter)}
+                                    style={{ fontSize: '100px', position: 'absolute', right: '30px', color: '#fff', backgroundColor: 'rgba(79, 182, 92, .3)', borderRadius: '8px', padding: '0 20px', cursor: 'pointer' }}
+                                >
+                                    {'>'}
+                                </span>
+                            </div>
+                        </section>
+                        <section style={{ width: '90%', margin: '120px auto', display: 'grid', gap: '30px', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+                            {
+                                loadedFoods?.map(
+                                    singleFood => (
+                                        <SingleFood
+                                            key={singleFood._id}
+                                            singleFood={singleFood}
+                                        />
+                                    )
+                                )
+                            }
+                        </section>
+                    </>
+                )
+                    : (
+                        <div style={{ height: '70vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            <h1 style={{ color: 'rgb(79, 182, 92)' }}>Sorry, there is no food available for this variant in our shop!!!</h1>
+                        </div>
                     )
-                }
-            </section>
+            }
+
         </>
     );
 };
